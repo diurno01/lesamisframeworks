80aa643b5bdd30fc775fd2426bf32c8e1e1a6a51
diurno01 <diurno1003@gmail.com>
2021-12-08 00:30:58 -0300

    ReservaHotelRepository arreglado

--- src/main/java/com/lesamis/lesAmisSpring/repositories/ReservaHotelRepository.java
+++ src/main/java/com/lesamis/lesAmisSpring/repositories/ReservaHotelRepository.java
@@ -1,7 +1,8 @@
 package com.lesamis.lesAmisSpring.repositories;
+import com.lesamis.lesAmisSpring.models.ReservaHotelModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservaHotelRepository extends JpaRepository<ReservaHotelRepository, Long> {
+public interface ReservaHotelRepository extends JpaRepository<ReservaHotelModel, Long> {
 }
